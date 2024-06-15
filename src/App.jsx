989c9ab3d6f32b0ea07d865d05d0203d87388771
HJC <huchardjc@gmail.com>
2024-06-16 00:18:11 +0200

    ✨ F01: create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import LoginPage from './pages/loginPage/LoginPage'
 
 function App() {
 
   return (
-    <div>App</div>
+    <LoginPage/>
   )
 }
 
---
+++ src/pages/loginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+  return (
+    <div>
+        <h1>Bienvenue chez nous !</h1>
+        <br />
+        <h2>Connectez-vous</h2>
+        <form >
+            <input type="text" placeholder="Entre votre prénom..."/>
+            <button type="submit">Accèdez à votre espace</button>
+        </form>
+    </div>)
+}
