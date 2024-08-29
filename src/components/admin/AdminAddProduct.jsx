a5006308d1ad633ce1791a4daa48be044deab906
HJC <huchardjc@gmail.com>
2024-08-29 14:29:44 +0200

    ✨ F08 : AdminAddProductLayout

--- src/components/admin/AdminAddProduct.jsx
+++ src/components/admin/AdminAddProduct.jsx
@@ -1,5 +1,40 @@
+import styled from "styled-components";
+
 export default function AdminAddProduct() {
   return (
-    <div>AdminAddProduct</div>
+    <AdminAddProductStyled>
+      <div className="image">image</div>
+      <div className="form">
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <div>submit button</div>
+      </div>
+
+    </AdminAddProductStyled>
   )
 }
+
+const AdminAddProductStyled = styled.div`
+border: 1px solid red;
+display: flex;
+flex:1;
+
+  .image {
+    border: 1px solid green;
+  }
+  .form {
+    border: 1px solid blue;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+      input {
+        border: 1px solid darkblue;
+        flex: 0.25;
+      }
+      div {
+        flex: 0.25;
+      }
+  }
+  
+`;
