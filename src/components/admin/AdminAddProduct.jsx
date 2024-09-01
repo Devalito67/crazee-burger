ff0e1b918ab0884a7056d7b8fb0577c920070f1e
HJC <huchardjc@gmail.com>
2024-09-01 17:37:28 +0200

    ✨ F08: Add success submit message

--- src/components/admin/AdminAddProduct.jsx
+++ src/components/admin/AdminAddProduct.jsx
@@ -4,6 +4,7 @@ import { useContext, useState } from "react";
 import defaultImage from "../../../public/images/coming-soon.png";
 import Input from "./Input";
 import { getAdminInputsConfig } from "./getAdminInputsConfig";
+import { FiCheckCircle } from "react-icons/fi";
 
 export default function AdminAddProduct() {
   const { menu, setMenu } = useContext(OrderPageContext);
@@ -13,7 +14,8 @@ export default function AdminAddProduct() {
     title: "",
     price: 0,
   }
-  const [newProduct, setNewProduct] = useState(emptyProduct)
+  const [newProduct, setNewProduct] = useState(emptyProduct);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const inputs = getAdminInputsConfig;
 
   const handleFormSubmit = (e) => {
@@ -30,7 +32,9 @@ export default function AdminAddProduct() {
     const copyMenu = [updatedProduct, ...menu];
     setMenu(copyMenu);
     e.target.reset();
+    setIsSubmitted(true);
     setNewProduct(emptyProduct)
+    setTimeout(submitMessage, 2000)
   };
 
   const handleChange = (e, inputName) => {
@@ -41,15 +45,22 @@ export default function AdminAddProduct() {
     }));
   };
 
+  const submitMessage = () => {
+    setIsSubmitted(false)
+  }
+
 
   return (
     <AdminAddProductStyled>
-      <div className="image"> {newProduct.imageSource ? <img src={newProduct.imageSource} alt="imageProduct preview" /> : "Aucune image"}</div>
+      <div className="image"> {newProduct.imageSource ? <img src={newProduct.imageSource} alt="imageProduct preview" /> : <div className="emptyImage">
+      Aucune Image</div>}</div>
       <form className="form" onSubmit={handleFormSubmit}>
         {inputs.map((input) => (
           <Input key={input.key} name={input.name} placeholder={input.placeholder} icon={input.icon} onChange={(e) => handleChange(e, input.name)} />
         ))}
-        <div><button>Ajouter un nouveau produit au menu</button></div>
+        <div><button>Ajouter un nouveau produit au menu</button>{ isSubmitted && <div className="successMessage">
+      <FiCheckCircle /><span>Ajouté avec succès !</span>
+    </div>}</div>
       </form>
     </AdminAddProductStyled>
   )
@@ -64,16 +75,21 @@ width: 65%;
   .image {
     display: flex;
     flex-shrink: 0;
-    border: 1px solid #E4E5E9;
     width: 215px;
     height: 120px;
-    justify-content: center;
-    align-items: center;
-    color: #93A2B1;
       img {
         width: 100%;
         height: 100%;
         object-fit: contain;
+      }
+      .emptyImage {
+        display: flex;
+        color: #93A2B1;
+        border: 1px solid #E4E5E9;
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
       }
   }
   .form {
@@ -82,6 +98,7 @@ width: 65%;
     grid-template-rows: repeat(4, 1fr);
     width: 100%;
       div {
+        display: flex;
         align-items: center;
       }
       button {
@@ -93,6 +110,22 @@ width: 65%;
         font-weight: bold;
         font-size: 12px;
         border-radius: 5px;
+        border: 1px solid #60BD4F;
+        &:hover {
+          color: #60BD4F;
+          background-color: white;
+        }
+        &:active {
+          color: white;
+          background-color: #60BD4F;
+        }
+      }
+      .successMessage {
+        display: flex;
+        column-gap: 5px;
+        color: #60BD4F;
+        padding-left: 10px;
+        padding-right: 2px;
       }
   }
   
